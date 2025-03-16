90f50339312145c48683485070ff133a0ba6f8d2
Sudharsan00-R <redmexmo@gmail.com>
2025-03-16 13:59:52 +0530

    add:assets

---
+++ assets/js/feshion_script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+    var sidebar = document.querySelector(".side-navbar");
+    sidebar.style.display = (sidebar.style.display === "block") ? "none" : "block";
+}
+
+function toggleMenu() {
+    var sidebar = document.querySelector(".side-navbar");
+    sidebar.classList.toggle("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
